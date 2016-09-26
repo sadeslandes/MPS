c94d0ce6f93b0eacba78d8d8aea872891c7a53f2
Samuel Deslandes (Sam) <sadeslandes@gmail.com>
2016-09-26 00:00:08 -0400

    add schematic

--- Lab2/part1.c
+++ Lab2/part1.c
@@ -12,8 +12,6 @@
 #define EXTCLK      22118400    // External oscillator frequency in Hz
 #define SYSCLK      49766400    // Output of PLL derived from (EXTCLK * 9/4)
 #define BAUDRATE    115200      // UART baud rate in bps
-//#define BAUDRATE  19200       // UART baud rate in bps
-//__bit SW2press = 0;
 char butpress;
 
 //-------------------------------------------------------
@@ -86,7 +84,7 @@ void PORT_INIT(void){
 	XBR2    = 0x40;             // Enable Crossbar and weak pull-ups.
 	P0MDOUT = 0x01;             // P0.0 (TX0) is configured as Push-Pull for output
 	// P0.1 (RX0) is configure as Open-Drain input.
-	// P0.2 (SW2 through jumper wire) is configured as Open_Drain for input.
+	// P0.2 (pushbutton) is configured as Open_Drain for input.
 	P0      = 0x06;             // Additionally, set P0.0=0, P0.1=1, and P0.2=1.
 	SFRPAGE = SFRPAGE_SAVE;     // Restore SFR page.
 }
